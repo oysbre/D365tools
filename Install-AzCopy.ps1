5a7ac981889ec76b2029bb58affde246b2a30b9a
oysbre <95480044+oysbre@users.noreply.github.com>
2023-03-31 13:46:20 +0200

    Update Install-AzCopy.ps1

--- Install-AzCopy.ps1
+++ Install-AzCopy.ps1
@@ -3,16 +3,18 @@ If (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdenti
 #"No Administrative rights popup window asking user for Admin rights";$arguments = "& '" + $myinvocation.mycommand.definition + "'";Start-Process "$psHome\powershell.exe" -Verb runAs -ArgumentList $arguments;break
 }
 function InstallUpgrade-AzCopy {
+  $ErrorActionPreference = "SilentlyContinue"; #This will hide errors
   If (-not(test-path "$env:systemroot\AzCopy.exe")) -or ((& azcopy -h | select-string -pattern "newer version").length -gt 0){
-  $ProgressPreference = 'SilentlyContinue'
-  Invoke-WebRequest -Uri "https://aka.ms/downloadazcopy-v10-windows" -OutFile $env:temp\AzCopy.zip -UseBasicParsing
-  if (test-path $env:temp\AzCopy.zip){
-  Unblock-File $env:temp\AzCopy.zip
-  Expand-Archive $env:temp\AzCopy.zip $env:temp\AzCopy -Force
-  Get-ChildItem $env:temp\AzCopy\*\azcopy.exe | Move-Item -Destination "$env:systemroot\AzCopy.exe"
-  remove-item $env:temp\AzCopy.zip -force
-  remove-item $env:temp\AzCopy -force -Recurse
-  }#end if testpath
+    $ProgressPreference = 'SilentlyContinue'
+    Invoke-WebRequest -Uri "https://aka.ms/downloadazcopy-v10-windows" -OutFile $env:temp\AzCopy.zip -UseBasicParsing
+    if (test-path $env:temp\AzCopy.zip){
+      Unblock-File $env:temp\AzCopy.zip
+      Expand-Archive $env:temp\AzCopy.zip $env:temp\AzCopy -Force
+      Get-ChildItem $env:temp\AzCopy\*\azcopy.exe | Move-Item -Destination "$env:systemroot\AzCopy.exe"
+      Remove-Item $env:temp\AzCopy.zip -force
+      Remove-Item $env:temp\AzCopy -force -Recurse
+    }#end if testpath
   }#end if
-}#end function InstallUpgrade-AzCopy
+  $ErrorActionPreference = "Continue"; #Turning errors back on
+#End function InstallUpgrade-AzCopy
 InstallUpgrade-AzCopy
