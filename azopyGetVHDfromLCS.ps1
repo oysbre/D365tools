1f594ec696ab5a791d266777284d8bb14d0e102f
oysbre <95480044+oysbre@users.noreply.github.com>
2023-06-05 13:34:46 +0200

    Update azopyGetVHDfromLCS.ps1

--- azopyGetVHDfromLCS.ps1
+++ azopyGetVHDfromLCS.ps1
@@ -1,13 +1,17 @@
-#Powershellscript to download VHD from LCS Shared library using AzCopy. Requires Admin session.
-if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+#Powershellscript to download VHD from LCS Shared library using AzCopy. Set/change targetdir variable if needed.
+#Requires Admin session.
+If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+
+#Force https over TLS12 protocol
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 #Set target downloadpath. Default c:\temp
 $targetdir = "C:\temp"
-$D365VHDname = "D365VHD-10_0_29_part"
+$D365VHDname = "D365VHD-10_0_32_part"
 
 #Note! The URLs with SAS token below are "expired" and will not work.
 #Generate URL for each VHD file with SAS token from https://lcs.dynamics.com
-#Place SAS URLs in right order in variable $URLS starting from nr 1.
+#Place SAS URLs in right order in variable $URLS starting from part file nr 1.
 #Save the script and run it!
 $URLS = @(
 <#1#>"https://uswedpl1catalog.blob.core.windows.net/product-ax7productname/5e514086-fe66-4f43-9bb4-b098fc096c0d/AX7ProductName-12-e73dbcb9-0b37-4fc0-9dd2-3696b518e66a?sv=2018-03-28&sr=b&sig=Yv97NE2HkAQ5hifrd2dkpISPaScJyUubf3t%2Bz7ab2WE%3D&se=2023-03-30T07%3A56%3A05Z&sp=r" 
@@ -17,46 +21,43 @@ $URLS = @(
 
 )
 #--------------------------------------
-#Force https over TLS12 protocol
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
 #Begin
 function Get-UrlStatusCode([string] $Urlcheck) {
     try {  (Invoke-WebRequest -Uri $Urlcheck -UseBasicParsing -DisableKeepAlive -method head).StatusCode }
     catch [Net.WebException]  { [int]$_.Exception.Response.StatusCode  }
 }#end function URL check
 
-function Get-AzCopy(){
-    write-host "Installing/updating AzCopy to C:\Windows..." -ForegroundColor Yellow
-    remove-item $env:temp\AzCopy.zip -force -ea 0
+function InstallUpgrade-AzCopy {
+  $ErrorActionPreference = "SilentlyContinue"; #This will hide errors
+  If ((-not(test-path "$env:systemroot\AzCopy.exe")) -or ((& azcopy -h | select-string -pattern "newer version").length -gt 0)){
     $ProgressPreference = 'SilentlyContinue'
-    invoke-WebRequest -Uri "https://aka.ms/downloadazcopy-v10-windows" -OutFile $env:temp\AzCopy.zip -UseBasicParsing
-    Unblock-File $env:temp\AzCopy.zip
-    Expand-Archive $env:temp\AzCopy.zip $env:temp\AzCopy -Force
-    Get-ChildItem $env:temp\AzCopy\*\azcopy.exe | Move-Item -Destination "C:\windows\AzCopy.exe"
-    remove-item $env:temp\AzCopy.zip -force
-    remove-item $env:temp\AzCopy -force -Recurse
-}#end function AzCopy
+    Invoke-WebRequest -Uri "https://aka.ms/downloadazcopy-v10-windows" -OutFile $env:temp\AzCopy.zip -UseBasicParsing
+    if (test-path $env:temp\AzCopy.zip){
+      Unblock-File $env:temp\AzCopy.zip
+      Expand-Archive $env:temp\AzCopy.zip $env:temp\AzCopy -Force
+      Get-ChildItem $env:temp\AzCopy\*\azcopy.exe | Move-Item -Destination "$env:systemroot\AzCopy.exe"
+      Remove-Item $env:temp\AzCopy.zip -force
+      Remove-Item $env:temp\AzCopy -force -Recurse
+    }#end if testpath
+ }#end if
+  $ErrorActionPreference = "Continue"; #Turning errors back on
+}#End function InstallUpgrade-AzCopy
 
 if ((!(test-path $targetdir -ea 0)) -or ($targetdir -eq "<targetdir>")){
     write-host "Set/check variable '$targetdir' and try again." -ForegroundColor red;pause;exit
 }
 
-#Check available diskspace for VHD imagefiles 
+#Check available diskspace for compressed VHD imagefiles 
 $diskspace =  [math]::Round((Get-WmiObject -Class Win32_LogicalDisk  | ? {$_. DriveType -eq 3} | ? {$_.DeviceID -like $targetdir.substring(0,2)}|select FreeSpace).freespace/1GB,0)
 if ($diskspace -lt 32){
-    write-host "Not enough diskspace on $($targetdir.substring(0,2).ToUpper()) for VHD imagefiles. Need approx 29 GB for the VHD files. Extracted VHD image need 133 GB to extract the files." -ForegroundColor red;
+    write-host "Not enough diskspace on $($targetdir.substring(0,2).ToUpper()) for VHD imagefiles. Need approx 29 GB for the compressed VHD files. Extracted VHD image need additional 133GB." -ForegroundColor red;
     pause;
     exit
 }
 
 #Install/update AzCopy
-If (!(test-path "C:\windows\AzCopy.exe")){Get-AzCopy}
-else {
-    $azcopyupdate = & azcopy -h | select-string -pattern "newer version"
-    if ($azcopyupdate){Get-AzCopy}
-}#end AZcopy  
-
-if (!(test-path "C:\windows\AzCopy.exe")){write-host "AzCopy not installed. Run Powershellscript with Admin privilege.";pause;exit}
+InstallUpgrade-AzCopy  
 
 #Download files
 $i = 1
@@ -73,7 +74,6 @@ foreach ($url in $URLS){
             azcopy copy $url "$targetdir\$($D365VHDname)$i.rar"
             unblock-file "$targetdir\$($D365VHDname)$i.rar" 
         }
-     
     }#end if url check
     else {write-host "Check the SAS link $($URL). Error : " $statuscode -foregroundcolor Red}
 $i++ #iterate next number
