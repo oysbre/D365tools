be321987c266790dd33ac7767965a44e9abe73a8
oysbre <95480044+oysbre@users.noreply.github.com>
2023-06-16 10:48:32 +0200

    Update D365CHEtuning.ps1

--- D365CHEtuning.ps1
+++ D365CHEtuning.ps1
@@ -134,6 +134,7 @@ Register-ScheduledTask -TaskName 'WarmupD365' -InputObject $Task -User "System"
 
 #Set AppPool settings for AOSERVICE
 Import-Module WebAdministration
+$siteName = "AOSSERVICE"
 $AppPool = Get-Item IIS:\AppPools\AOSSERVICE
 if ($AppPool){
 #disable timeout
@@ -142,10 +143,61 @@ Set-ItemProperty ("IIS:\AppPools\AOSSERVICE") -Name processModel.idleTimeout -va
 Set-ItemProperty ("IIS:\AppPools\AOSSERVICE") -Name Recycling.periodicRestart.time -Value "00:00:00"
 #Clear any scheduled restart times
 Clear-ItemProperty ("IIS:\AppPools\AOSSERVICE") -Name Recycling.periodicRestart.schedule
-#set to alwaysrunning
-Set-ItemProperty ("IIS:\AppPools\AOSSERVICE") -Name startMode -Value "AlwaysRunning"
 }
 
+#Enable IIS Application Initialization
+#Ensure Application Initialization is available
+$webAppInit = Get-WindowsFeature -Name "Web-AppInit"
+if(!$webAppInit.Installed) 
+{
+    Write-Host "$($webAppInit.DisplayName) not present, installing"
+    Install-WindowsFeature $webAppInit -ErrorAction Stop
+    Write-Host "`nInstalled $($webAppInit.DisplayName)`n" -ForegroundColor Green
+}
+else 
+{
+    Write-Host "$($webAppInit.DisplayName) was already installed" -ForegroundColor Yellow
+}
+
+#Fetch the site
+$site = Get-Website -Name $siteName
+
+if(!$site)
+{
+    Write-Host "Site $siteName could not be found, exiting!" -ForegroundColor Yellow
+    Break
+}
+
+
+#Fetch the application pool
+$appPool = Get-ChildItem IIS:\AppPools\ | Where-Object { $_.Name -eq $site.applicationPool }
+
+
+#Set up AlwaysRunning
+if($appPool.startMode -ne "AlwaysRunning")
+{
+    Write-Host "startMode is set to $($appPool.startMode ), activating AlwaysRunning"
+    
+    $appPool | Set-ItemProperty -name "startMode" -Value "AlwaysRunning"
+    $appPool = Get-ChildItem IIS:\AppPools\ | Where-Object { $_.Name -eq $site.applicationPool }
+
+    Write-Host "startMode is now set to $($appPool.startMode)`n" -ForegroundColor Green
+} 
+else 
+{
+    Write-Host "startMode was already set to $($appPool.startMode) for the application pool $($site.applicationPool)" -ForegroundColor Yellow
+}
+
+if(!(Get-ItemProperty "IIS:\Sites\$siteName" -Name applicationDefaults.preloadEnabled).Value) 
+{
+    Write-Host "preloadEnabled is inactive, activating"
+    Set-ItemProperty "IIS:\Sites\$siteName" -Name applicationDefaults.preloadEnabled -Value True
+    Write-Host "preloadEnabled is now set to $((Get-ItemProperty "IIS:\Sites\$siteName" -Name applicationDefaults.preloadEnabled).Value)" -ForegroundColor Green
+} 
+else
+{
+    Write-Host "preloadEnabled already active" -ForegroundColor Yellow
+}
 
 #Install Nuget
 If (((Get-PackageProvider -listavailable).name).contains("NuGet") -eq $false){
