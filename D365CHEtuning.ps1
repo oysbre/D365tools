a832dabb9156d496e19d3962a14b625a57e423ad
oysbre <95480044+oysbre@users.noreply.github.com>
2023-10-31 13:35:58 +0100

    Update D365CHEtuning.ps1
    added MS Visual C 2022

--- D365CHEtuning.ps1
+++ D365CHEtuning.ps1
@@ -114,24 +114,34 @@ Set-Content -Path "$DesktopPath\UnsetReadonlyFlag.ps1" -Value $unsetcmd
 Set-Content -Path "$DesktopPath\StopServices.ps1" -Value $StopServicesCmd
 Set-Content -Path "$DesktopPath\StartServices.ps1" -Value $StartServicesCmd
 
-#create a Scheduletask and warmupscript under c:\D365scripts to run "Warmupscript" after startup
-$warmupscript = @'
-#Warmup D365 env
-Invoke-WebRequest -Uri (get-d365url).url -UseDefaultCredentials
-'@
-if (-not(test-path "c:\D365scripts")){new-item -ItemType directory -Path "c:\D365scripts"}
-if (-not(test-path c:\D365scripts\WarmupD365.ps1)){
-Unregister-ScheduledTask -TaskName 'WarmupD365' -ea 0
-remove-item "c:\D365scripts\WarmupD365.ps1" -force -ea 0
-$warmupscript | out-file -filepath c:\D365scripts\WarmupD365.ps1 -encoding utf8 -force -Width 2147483647
-[string]$sch_args = '-executionpolicy bypass -NonInteractive -NoLogo -NoProfile -File "c:\D365scripts\WarmupD365.ps1"'
-$Action = New-ScheduledTaskAction -Execute '%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe' -Argument $sch_args
-$Trigger = New-ScheduledTaskTrigger -atstartup
-$Trigger.Delay = 'PT30S' #delay for 30 sec after startup
-$Settings = New-ScheduledTaskSettingsSet
-$Task = New-ScheduledTask -Action $Action -Trigger $Trigger -Settings $Settings
-Register-ScheduledTask -TaskName 'WarmupD365' -InputObject $Task -User "System"
+# MS Visual C++ 2022 redist
+$DownloadPath = "$env:temp"
+$FilesToDownload = @(
+'https://aka.ms/vs/17/release/VC_redist.x64.exe'
+)
+
+$FilesToDownload  |
+ForEach{
+    $webclient = New-Object System.Net.WebClient
+    $url       = $PSItem
+    $filename  = [System.IO.Path]::GetFileName($url)
+    $file      = "$DownloadPath\$filename"
+    $webclient.DownloadFile($url, $file)
+    
 }
+if (test-path $file){
+$vcdlver = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($file).FileVersion
+$vclibver = gci "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\" -ea 0| get-itemproperty | where-object {$_.displayname -like "Microsoft Visual C*2022*"} |   Select-Object DisplayName, displayversion | sort-object -property displayversion -Descending | select -First 1
+    
+    if (($vclibver.DisplayVersion -lt $vcdlver) -or ($vclibver -eq $NULL)){
+       write-host "Installing MS Visual C++ 2022 ver $($vcdlver)" -ForegroundColor yellow
+       $vcargs = "/q /norestart"
+        Start-Process $file -Wait -ArgumentList $vcargs
+        remove-item $file -force
+    }#end if ver check
+
+}#end if VcDlfile check
+else {write-host "MS Visual C++ download not found in $($DownloadPath). Newer deploypackages may fail" -ForegroundColor red;}
 
 #Set AppPool settings for AOSERVICE
 Import-Module WebAdministration
