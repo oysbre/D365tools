a28d0c90ac65ba7b28e309973f8a1ea0c95dbf30
oysbre <95480044+oysbre@users.noreply.github.com>
2024-12-09 08:40:55 +0100

    Update SqlAuthenticationProviders-fix.ps1

--- SqlAuthenticationProviders-fix.ps1
+++ SqlAuthenticationProviders-fix.ps1
@@ -1,7 +1,7 @@
 <#
 Issue 964534 CHE update failed on step 38 GlobalUpdate script for service model: MROneBox
 Copy the Powershell command below, run it in a Powershell console on the CHE devbox to download script to Desktop
-The run the powershellscript located on the Desktop to add SqlAuthenticationProviders in web.config if it's missing.
+Run the powershellscript located on the Desktop to add SqlAuthenticationProviders in web.config if it's missing.
 iwr https://raw.githubusercontent.com/oysbre/D365tools/main/SqlAuthenticationProviders-fix.ps1 -outfile "$env:USERPROFILE\Desktop\SqlAuthenticationProviders-fix.ps1"
 #>
 
