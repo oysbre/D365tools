2395b47cb8d72a42393391bcbdd39b6b18e88524
oysbre <95480044+oysbre@users.noreply.github.com>
2025-03-07 08:17:38 +0100

    Update D365CHEtuning.ps1

--- D365CHEtuning.ps1
+++ D365CHEtuning.ps1
@@ -25,11 +25,7 @@ Write-host "This script runs several optimization settings for the CHE environme
 #Install PowershellGet,Nuget and D365fo.tools
 Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
 
-If (((Get-PackageProvider -listavailable).name).contains("NuGet") -eq $false){
-	Write-host "Installing NuGet..." -foregroundcolor yellow
-	Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-}
-
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Import-PackageProvider -Name NuGet 
 
 if ((get-module -name PowerShellGet) -eq $null){
