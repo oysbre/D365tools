34dcd0d83dad6837b2981c2d85ae890ff563cb46
oysbre <95480044+oysbre@users.noreply.github.com>
2025-03-19 09:39:34 +0100

    Update CipherSuites.ps1

--- CipherSuites.ps1
+++ CipherSuites.ps1
@@ -35,4 +35,10 @@ foreach ($c in $cs) {
     }
     $cspos = $cspos+1
 }#end foreach cipher
+
+#Set strong cryptography on 64 bit .Net Framework (version 4 and above)
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NetFramework\v4.0.30319' -Name 'SchUseStrongCrypto' -Value '1' -Type DWord
+#set strong cryptography on 32 bit .Net Framework (version 4 and above)
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\.NetFramework\v4.0.30319' -Name 'SchUseStrongCrypto' -Value '1' -Type DWord
+
 Write-host "Reboot computer to use new ciphersuites..." -foregroundcolor Yellow
