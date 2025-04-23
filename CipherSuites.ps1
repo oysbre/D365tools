81c66300617f7318e7a9a8c46929ba0cb0ae08b3
oysbre <95480044+oysbre@users.noreply.github.com>
2025-04-23 12:11:07 +0200

    Update CipherSuites.ps1

--- CipherSuites.ps1
+++ CipherSuites.ps1
@@ -7,30 +7,33 @@ Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSComm
 $ErrorActionPreference = 'Stop';
 $regPath = 'HKLM:\SOFTWARE\Policies\Microsoft\Cryptography\Configuration\SSL\00010002';
 $ciphers = Get-ItemPropertyValue "$regPath" -Name 'Functions';
-Write-host "Values before: $ciphers";
 $cipherList = $ciphers.Split(',');
 #Set strong cryptography on 64 bit .Net Framework (version 4 and above)
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NetFramework\v4.0.30319' -Name 'SchUseStrongCrypto' -Value '1' -Type DWord
 #set strong cryptography on 32 bit .Net Framework (version 4 and above)
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\.NetFramework\v4.0.30319' -Name 'SchUseStrongCrypto' -Value '1' -Type DWord
 $updateReg = $false;
-if ($cipherList -inotcontains 'TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384') {
-    Write-Host "Adding TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384";
-    $ciphers += ',TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384';
-    $updateReg = $true;
-}
+Write-host "Values before: $ciphers" -ForegroundColor cyan;
 if ($cipherList -inotcontains 'TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA256') {
     Write-Host "Adding TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA256";
-    $ciphers += ',TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA256';
+    #$ciphers += ',TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA256';
+    $ciphers = $ciphers.insert(0,'TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA256,')
     $updateReg = $true;
 }
-if ($updateReg) {
-    Set-ItemProperty "$regPath" -Name 'Functions' -Value "$ciphers";
-    $ciphers = Get-ItemPropertyValue "$regPath" -Name 'Functions';
-    write-host "Values after: $ciphers";
-    Write-host "Reboot computer to use new ciphersuites..." -foregroundcolor Yellow
+if ($cipherList -inotcontains 'TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384') {
+    Write-Host "Adding TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384";
+    #$ciphers += ',TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384';
+    $ciphers = $ciphers.insert(0,'TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,')
+    $updateReg = $true;
 }
 
+if ($updateReg) {
+    
+    Set-ItemProperty "$regPath" -Name 'Functions' -Value "$ciphers";
+    $ciphers = Get-ItemPropertyValue "$regPath" -Name 'Functions';
+    write-host "Values after: $ciphers" -ForegroundColor green;
+    Write-host "Reboot computer to use new ciphersuites..." -foregroundcolor Yellow
+}
 
 
 
