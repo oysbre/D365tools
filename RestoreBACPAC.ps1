0442fd23ff5a9f401143baaf97c0cfaca119b889
oysbre <95480044+oysbre@users.noreply.github.com>
2025-06-06 08:52:00 +0200

    Update RestoreBACPAC.ps1

--- RestoreBACPAC.ps1
+++ RestoreBACPAC.ps1
@@ -1,5 +1,5 @@
 #Script to restore BACPAC on D365 CHE. 
-#To download Powershellscript to Desktop, run Powershell command below. Run with Powershell after download.
+#To download this Powershellscript to Desktop, run Powershell command below. Edit the script and fill in SASURL variable. Run with Powershell when done.
 # iwr https://raw.githubusercontent.com/oysbre/D365tools/refs/heads/main/RestoreBACPAC.ps1 -outfile "$env:USERPROFILE\Desktop\RestoreBACPAC.ps1"
 If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {   
 #"No Administrative rights, it will display a popup window asking user for Admin rights"
