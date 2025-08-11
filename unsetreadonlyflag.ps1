84de0d8d8bef6f96a675197edf4bced1f703e118
oysbre <95480044+oysbre@users.noreply.github.com>
2025-08-11 09:03:09 +0200

    Update unsetreadonlyflag.ps1

--- unsetreadonlyflag.ps1
+++ unsetreadonlyflag.ps1
@@ -7,7 +7,7 @@ $unsetcmd = @'
 #Unset ReadOnly flag on multiple fileextensions in Powershell (run as Admin):
 If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")){$arguments = "& '" + $myinvocation.mycommand.definition + "'";Start-Process "$psHome\powershell.exe" -Verb runAs -ArgumentList $arguments;break}
 Write-Host 'Unset ReadOnly flag on fileextentions .rdl, .log, .xml and .txt in PackagesLocalDirectory. Please wait...' -foregroundcolor Yellow
-@("*.rdl","*.log","*.xml","*.txt") | foreach {Get-ChildItem -Path "$env:servicedrive\AosService\PackagesLocalDirectory\*" -Recurse -Filter "$_" | foreach { $_.IsReadOnly=$False }}
+@("*.rdl","*.log","*.xml","*.txt") | foreach {Get-ChildItem -Path "$env:servicedrive\AosService\PackagesLocalDirectory\*" -Recurse -Filter "$_" -ReadOnly | foreach { $_.IsReadOnly=$False }}
 '@
 
 Write-host "Creating powershellscript on Desktop that unset READONLY flag on file extensions .rdl, .log, .xml and .txt in packageslocaldirectory." -foregroundcolor yellow
