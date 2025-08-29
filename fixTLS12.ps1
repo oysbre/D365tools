759a8e7751951665511d3aa629f5778eb72447f1
oysbre <95480044+oysbre@users.noreply.github.com>
2025-08-29 09:26:44 +0200

    Update fixTLS12.ps1

--- fixTLS12.ps1
+++ fixTLS12.ps1
@@ -264,7 +264,41 @@ if ([System.Version]$os.Version -lt [System.Version]'10.0') {
     'TLS_RSA_WITH_AES_256_CBC_SHA',
     'TLS_RSA_WITH_AES_128_CBC_SHA'
   )
-} else {
+ }
+ #Windows Server 2022
+ else if  ([System.Version]$os.Version -eq [System.Version]'10.0.20348') {
+ Write-Host 'Use cipher suites order for Windows 2022.'
+  $cipherSuitesOrder = @(
+  'TLS_CHACHA20_POLY1305_SHA256',
+  'TLS_AES_256_GCM_SHA384',
+  'TLS_AES_128_GCM_SHA256',
+  'TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384',
+  'TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256',
+  'TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384',
+  'TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256',
+  'TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384',
+  'TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256',
+  'TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA384',
+  'TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256'
+  )
+}
+ #Windows Server 2019
+ else if  ([System.Version]$os.Version -eq [System.Version]'10.0.17763') {
+ Write-Host 'Use cipher suites order for Windows 2019.'
+  $cipherSuitesOrder = @(
+  'TLS_AES_256_GCM_SHA384',
+  'TLS_AES_128_GCM_SHA256',
+  'TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384',
+  'TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256',
+  'TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384',
+  'TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256',
+  'TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384',
+  'TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256',
+  'TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA384',
+  'TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256'
+  )
+}
+else {
   Write-Host 'Use cipher suites order for Windows 10/2016 and later.'
   $cipherSuitesOrder = @(
     'TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384',
