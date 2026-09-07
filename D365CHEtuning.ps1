3f4636f193373084c532bd22b73239dc99686ba1
oysbre <95480044+oysbre@users.noreply.github.com>
2026-09-07 10:21:49 +0200

    Update D365CHEtuning.ps1

--- D365CHEtuning.ps1
+++ D365CHEtuning.ps1
@@ -252,6 +252,7 @@ write-host "Removing READONLY flag on file extensions RDL, LOG, XML and TXT in A
 
 $StopServicesCmd = @'
 If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")){$arguments = "& '" + $myinvocation.mycommand.definition + "'";Start-Process "$psHome\powershell.exe" -Verb runAs -ArgumentList $arguments;break}
+Write-host "Stopping D365 related services..." -foregroundcolor yellow
 @("MR2012ProcessService","DynamicsAxBatch","Microsoft.Dynamics.AX.Framework.Tools.DMF.SSISHelperService.exe","W3SVC")| foreach {stop-service -name "$_" -force}
 '@
 
